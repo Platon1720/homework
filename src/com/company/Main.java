7fc5645148e9a01932ee7557851be6bd895fedd8
Platon1720 <vanokonovalenko123123@gmail.com>
2020-08-20 16:25:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000L;
+        float f = 100.5f;
+        double d = 1000.5;
+        char ch = 'a';
+        boolean b2 = true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(b2);
+
+
+
+
+
+    }
+}
